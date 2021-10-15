8575524206bd4e77b7b1e5b09b316c85308621c3
Lucy Dykhouse <l.dykhouse24@gmail.com>
2021-10-15 15:34:07 -0400

    Finished sldc.js

--- sldc.js
+++ sldc.js
@@ -5,15 +5,22 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
+// Planning
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
+
+let planning = "When developers plan for the project ahead, including the project scope and schedule.";
+let analysisOfRequirements = "When developers gather the information required for the project, such as user needs or software requirements.";
+let design = "When developers draft the details for the end product, such as its UI or databases.";
+let implementation = "When developers write the code to build the product that they designed.";
+let testingAndIntegration = "When developers assess their product to ensure there are no bugs. The product can then be integrated into its environment and reach the market.";
+let maintain = "When developers address issues that are raised by the product's end-users.";
